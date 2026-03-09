02b16943bc74b35560dd38e99c671f224b58f327
Stray1144 <stray1144@gmail.com>
2026-03-09 18:32:36 -0500

    context: logger initialization

--- source/3asm.c
+++ source/3asm.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include "3asm.h"
+#include <libvacant/libvacant.h>
+
+void context_minimal_clear(context_t *context) {
+        if(context == nullptr) return;
+
+        logger_clear(&context->logger);
+}
+
+bool context_minimal_init(context_t *context) {
+        if(context == nullptr) return false;
+
+        context_minimal_clear(context);
+
+        logger_init(&context->logger, LOGGER_WARN);
+
+        return true;
+}
 
 int main(void) {
-        printf("hello world\n");
+        context_t context = {0};
+        context_minimal_init(&context);
 
+        context_minimal_clear(&context);
 }
