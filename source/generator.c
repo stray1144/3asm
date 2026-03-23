ee3638e0b2b17bdbcf87cece2beefdd4b7ddcb93
Stray1144 <stray1144@gmail.com>
2026-03-23 17:06:11 -0500

    generator: removed unused variable

--- source/generator.c
+++ source/generator.c
@@ -122,7 +122,6 @@ generator_result_t generate(translation_unit_t *output, semantizer_t *source) {
         generator_result_t result = generator_result(GENERATOR_OK, 0);
 
         for(uint32_t i = 0; i < semantizer_stream_size(source); i++) {
-                semantizer_unit_kind_t kind = semantizer_stream_get(source, i);
                 result = generator_process(output, source, i);
                 if(result.status != GENERATOR_OK) break;
         }
